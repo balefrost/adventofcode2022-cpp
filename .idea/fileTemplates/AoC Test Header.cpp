82fdab08f0253b4294f1fcf94f1b50fbd463c65e
Dan Yankowsky <bytecrafter@gmail.com>
2022-12-05 09:34:46 -0500

    file template

---
+++ .idea/fileTemplates/AoC Test Header.cpp
@@ -0,0 +1,12 @@
+#[[#include]]# <gtest/gtest.h>
+#[[#include]]# <fstream>
+
+TEST(Day${DAY_NUMBER}, Part1) {
+    std::ifstream input;
+    input.open("../../test/input/day${DAY_NUMBER}.txt");
+}
+
+TEST(Day${DAY_NUMBER}, Part2) {
+    std::ifstream input;
+    input.open("../../test/input/day${DAY_NUMBER}.txt");
+}
