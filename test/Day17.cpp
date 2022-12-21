d6ad642b3b2e872a59da3206d9f81674e3ffdf73
Dan Yankowsky <bytecrafter@gmail.com>
2022-12-20 19:57:11 -0500

    day17 part2

--- test/Day17.cpp
+++ test/Day17.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <fstream>
 
+#include "util.h"
+
 using namespace std;
 
 const string sample_input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
@@ -28,12 +30,24 @@ T sign(T value) {
            0;
 }
 
+bool board_get(const vector<byte> &board, size_t bx, size_t by) {
+    return ((board[by] >> bx) & (byte) 0x01) != (byte) 0x00;
+}
+
+void board_set(vector<byte> &board, size_t bx, size_t by, bool value) {
+    if (value) {
+        board[by] |= (byte) (1 << bx);
+    } else {
+        board[by] &= (byte) (~(1 << bx));
+    }
+}
+
 bool iterate_piece(const vector<string> &piece,
-                   int px, int py,
-                   function<bool(int bx, int by, int px, int py)> f) {
+                   size_t px, size_t py,
+                   const function<bool(size_t bx, size_t by, size_t px, size_t py)> &f) {
     for (auto r = 0; r < piece.size(); ++r) {
         auto br = py + r;
-        auto pr = piece.size() - 1 - r;
+        size_t pr = piece.size() - 1 - r;
         auto &piece_row = piece[piece.size() - 1 - r];
         for (auto c = 0; c < piece[r].size(); ++c) {
             auto bc = px + c;
@@ -48,32 +62,68 @@ bool iterate_piece(const vector<string> &piece,
     return true;
 }
 
-void print_top_of_board(const vector<string> &board, int n) {
+void print_top_of_board(const vector<byte> &board, int n) {
     for (auto i = 0; i < n; ++i) {
         if (i >= board.size()) {
             break;
         }
         auto row = board.size() - 1 - i;
-        cout << board[row] << endl;
+        for (auto x = 0; x < 7; ++x) {
+            cout << (board_get(board, x, row) ? '#' : '.');
+        }
+        cout << endl;
     }
 }
 
-bool can_move(const vector<string> &board,
+bool can_move(const vector<byte> &board,
               const vector<string> &piece,
-              int px, int py,
+              size_t px, size_t py,
               int dx, int dy) {
-    return iterate_piece(piece, px, py, [&board, dx, dy](int bx, int by, int px, int py){
+    return iterate_piece(piece, px, py, [&board, dx, dy](int bx, int by, int px, int py) {
         auto tbx = bx + sign(dx);
         auto tby = by + sign(dy);
-        auto& board_row = board[tby];
-        if (tby < 0 || tbx < 0 || tbx >= board.front().size()) {
+        if (tby < 0 || tbx < 0 || tbx >= 7) {
             return false;
         }
-        if (board_row[tbx] == '#') {
+        if (board_get(board, tbx, tby)) {
             return false;
         }
         return true;
     });
+}
+
+void drop_piece(vector<byte> &board,
+                const vector<string> &piece,
+                const string &wind,
+                size_t &top,
+                size_t &wind_index) {
+    auto py = top + 3;
+    auto px = 2;
+    auto curr_sym_height = piece.size();
+    auto needed_rows = top + 3 + curr_sym_height;
+    while (board.size() < needed_rows) {
+        board.push_back((byte) 0);
+    }
+
+    while (true) {
+        auto shift_dir = wind[wind_index];
+        wind_index = (wind_index + 1) % wind.size();
+        auto dx = shift_dir == '<' ? -1 : 1;
+        if (can_move(board, piece, px, py, dx, 0)) {
+            px += dx;
+        }
+
+        if (can_move(board, piece, px, py, 0, -1)) {
+            py -= 1;
+        } else {
+            iterate_piece(piece, px, py, [&](size_t bx, size_t by, size_t px, size_t py) {
+                top = max(top, by + 1);
+                board_set(board, bx, by, true);
+                return true;
+            });
+            break;
+        }
+    }
 }
 
 TEST(Day17, Part1) {
@@ -84,51 +134,172 @@ TEST(Day17, Part1) {
     string wind;
     getline(input, wind);
 
-    vector<string> board;
-    auto top = -1;
+    vector<byte> board;
+    size_t top = 0;
 
-    size_t j = 0;
+    size_t windex = 0;
     for (auto i = 0; i < 2022; ++i) {
         auto &curr_piece = pieces[i % pieces.size()];
-        auto curr_sym_height = curr_piece.size();
-        auto needed_rows = top + 4 + curr_sym_height;
-        while (board.size() < needed_rows) {
-            board.emplace_back(7, '.');
+        drop_piece(board, curr_piece, wind, top, windex);
+    }
+
+    cout << top << endl;
+}
+
+void compress_board(vector<byte> &board, size_t &top) {
+    board.erase(board.begin() + top, board.end());
+
+    if (top == 0) {
+        return;
+    }
+
+    typedef pair<size_t, size_t> pos;
+    set<pos> frontier;
+    set<pos> seen;
+    for (auto i = 0; i < 7; ++i) {
+        if (!board_get(board, i, top - 1)) {
+            frontier.insert({i, top - 1});
         }
-
-        auto piece_row = top + 4;
-        auto piece_col = 2;
-
-        while (true) {
-            auto shift_dir = wind[j];
-            j = (j + 1) % wind.size();
-            auto dx = shift_dir == '<' ? -1 : 1;
-            if (can_move(board, curr_piece, piece_col, piece_row, dx, 0)) {
-                piece_col += dx;
+    }
+    auto lowest = top - 1;
+    while (!frontier.empty()) {
+        auto f = *frontier.begin();
+        frontier.erase(frontier.begin());
+        if (seen.find(f) == seen.end()) {
+            seen.insert(f);
+            lowest = min(lowest, f.second);
+            if (f.first > 0) {
+                if (!board_get(board, f.first - 1, f.second)) {
+                    frontier.insert({f.first - 1, f.second});
+                }
             }
-
-            if (can_move(board, curr_piece, piece_col, piece_row, 0, -1)) {
-                piece_row -= 1;
-            } else {
-                iterate_piece(curr_piece, piece_col, piece_row, [&](int bx, int by, int px, int py){
-                    top = max(top, by);
-                    board[by][bx] = curr_piece[py][px];
-                    return true;
-                });
-//                print_top_of_board(board, 10);
-//                cout << endl;
-                break;
+            if (f.first < 6) {
+                if (!board_get(board, f.first + 1, f.second)) {
+                    frontier.insert({f.first + 1, f.second});
+                }
+            }
+            if (f.second > 0) {
+                if (!board_get(board, f.first, f.second - 1)) {
+                    frontier.insert({f.first, f.second - 1});
+                }
             }
         }
     }
 
-//    cout << "count: " << std::count_if(board.begin(), board.end(), [](auto s) { return s == "#######"; }) << endl;
+    if (lowest > 0) {
+        board.erase(board.begin(), board.begin() + lowest);
+    }
 
-    cout << (top + 1) << endl;
+    top -= lowest;
 }
+
+template<typename T>
+struct item_comparer {
+    vector<function<strong_ordering(const T &, const T &)>> comparisons;
+
+    struct less_comparer_func {
+        vector<function<strong_ordering(const T &, const T &)>> comparisons;
+
+        bool operator()(const T &a, const T &b) {
+            for (const auto &item: comparisons) {
+                auto comparison_result = item(a, b);
+                if (comparison_result == strong_ordering::less) {
+                    return true;
+                } else if (comparison_result == strong_ordering::greater) {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    };
+
+    template<typename R>
+    item_comparer &then_by(function<R(T)> f) {
+        comparisons.push_back([f](const T &a, const T &b) {
+            return f(a) <=> f(b);
+        });
+        return *this;
+    }
+
+    template<typename R>
+    item_comparer &then_by(R T::* f) {
+        comparisons.push_back([f](const T &a, const T &b) {
+            return a.*f <=> b.*f;
+        });
+        return *this;
+    }
+
+    less_comparer_func as_less() {
+        return less_comparer_func{comparisons};
+    }
+};
 
 TEST(Day17, Part2) {
     ifstream input;
     input.open("../../test/input/day17.txt");
-    //stringstream input(sample_input);
+//    stringstream input(sample_input);
+
+    string wind;
+    getline(input, wind);
+
+    vector<byte> board;
+
+    struct state {
+        int pindex;
+        size_t windex;
+        vector<byte> board;
+        size_t top;
+        size_t height_increased_from_last_state;
+    };
+
+    auto state_comparer = item_comparer<state>()
+            .then_by(&state::pindex)
+            .then_by(&state::windex)
+            .then_by(&state::board)
+            .then_by(&state::top)
+            .then_by(&state::height_increased_from_last_state)
+            .as_less();
+
+    auto p = iterate_until_cycle<state>(
+            state{0, 0, board, 0, 0},
+            [&board, &wind](state s) {
+                auto &curr_piece = pieces[s.pindex];
+                s.pindex = (s.pindex + 1) % pieces.size();
+                auto old_top = s.top;
+                drop_piece(board, curr_piece, wind, s.top, s.windex);
+                s.height_increased_from_last_state = s.top - old_top;
+                compress_board(board, s.top);
+                return s;
+            },
+            state_comparer);
+
+    cout << p.first.size() << "  " << p.second.size() << endl;
+
+    unsigned long long remaining_pieces = 1000000000000;
+    unsigned long long total_height = 0;
+    unsigned long long height_for_prefix = 0;
+    for (const auto &item: p.first) {
+        height_for_prefix += item.height_increased_from_last_state;
+    }
+
+    unsigned long long height_for_cycle = 0;
+    for (const auto &item: p.second) {
+        height_for_cycle += item.height_increased_from_last_state;
+    }
+
+    total_height += height_for_prefix;
+    remaining_pieces -= max((size_t) 0, p.first.size() - 1);  //the initial state is included in p.first
+
+    auto total_cycles = remaining_pieces / p.second.size();
+    total_height += total_cycles * height_for_cycle;
+    remaining_pieces -= total_cycles * p.second.size();
+
+    auto iter = p.second.begin();
+    for (auto i = 0; i < remaining_pieces; ++i) {
+        total_height += p.second[i].height_increased_from_last_state;
+        ++iter;
+    }
+
+    cout << total_height << endl;
 }
--- test/util.h
+++ test/util.h
@@ -2,165 +2,196 @@
 
 #include <functional>
 #include <sstream>
+#include <stack>
 #include <string>
 #include <vector>
 
-template<typename Iter>
-std::string string_join(Iter begin, Iter end, const std::string &joiner = ", ") {
-    std::stringstream result;
-    bool first = true;
-    for (auto current = begin; current != end; ++current) {
-        if (!first) {
-            result << joiner;
-        }
-        result << *current;
-        first = false;
-    }
-    return result.str();
-}
-
-template<typename T>
-class postorder {
-public:
-    postorder(
-            T root,
-            const std::function<std::vector<T>(T elem)> &find_adjacent
-    ) : root_(std::move(root)), find_adjacent_(find_adjacent) {
-
-    }
-
-    struct postorder_stack_elem {
-        std::vector<T> elems;
-        size_t current_index{0};
-
-        [[nodiscard]] bool at_end() const { return current_index == elems.size(); }
-
-        T &current_elem() {
-            return elems.at(current_index);
-        }
-
-        bool operator==(const postorder_stack_elem &b) {
-            return elems == b.elems && current_index == b.current_index;
-        }
-    };
-
-    struct postorder_iter {
-    private:
-        std::stack<postorder_stack_elem> stack_{};
-        std::function<std::vector<T>(const T &elem)> find_adjacent_;
-
-        void fill_stack() {
-            if (!stack_.empty()) {
-                while (true) {
-                    auto adj = find_adjacent_(stack_.top().current_elem());
-                    if (adj.empty()) {
-                        break;
-                    }
-                    stack_.push({adj, 0});
-                }
-            }
-        }
-
-    public:
-        postorder_iter(std::stack<postorder_stack_elem> stack,
-                       std::function<std::vector<T>(const T &elem)> find_adjacent) : stack_(stack), find_adjacent_(
-                std::move(find_adjacent)) {
-            fill_stack();
-        }
-
-        bool operator==(const postorder_iter &rhs) const {
-            return stack_ == rhs.stack_;
-        }
-
-        bool operator!=(const postorder_iter &rhs) const {
-            return !(*this == rhs);
-        }
-
-        T *operator->() {
-            if (stack_.empty()) {
-                throw std::logic_error("invalid dereference");
-            }
-            postorder_stack_elem &st = stack_.top();
-            return &(st.elems.at(st.current_index));
-        };
-
-        T &operator*() {
-            if (stack_.empty()) {
-                throw std::logic_error("invalid dereference");
-            }
-            return stack_.top().elems.at(stack_.top().current_index);
-        }
-
-        postorder_iter &operator++() {
-            if (!stack_.empty()) {
-                stack_.top().current_index++;
-                if (stack_.top().at_end()) {
-                    stack_.pop();
-                } else {
-                    fill_stack();
-                };
-            }
-            return *this;
-        }
-    };
-
-    [[nodiscard]] postorder_iter begin() const {
-        std::stack<postorder_stack_elem> s;
-        s.push({{root_}, 0});
-        return postorder_iter{std::move(s), find_adjacent_};
-    }
-
-    [[nodiscard]] postorder_iter end() const {
-        return postorder_iter{std::stack<postorder_stack_elem>(), find_adjacent_};
-    }
-
-private:
-    T root_;
-    std::function<std::vector<T>(T elem)> find_adjacent_;
-};
-
-template<typename Iter>
-std::vector<std::pair<std::set<typename std::iterator_traits<Iter>::value_type>,std::set<typename std::iterator_traits<Iter>::value_type>>>
-find_partitions(const Iter &begin, const Iter &end) {
+inline namespace {
     using namespace std;
 
-    typedef typename iterator_traits<Iter>::value_type value_type;
-
-    vector<bool> included(distance(begin, end));
-
-    vector<pair<set<value_type>,set<value_type>>> result;
-
-    while (true) {
-        set<value_type> p1;
-        set<value_type> p2;
-        Iter item_curr = begin;
-        auto incl_curr = included.begin();
-        while (item_curr != end) {
-            if (*incl_curr) {
-                p1.insert(*item_curr);
-            } else {
-                p2.insert(*item_curr);
+    template<typename Iter>
+    string string_join(Iter begin, Iter end, const string &joiner = ", ") {
+        stringstream result;
+        bool first = true;
+        for (auto current = begin; current != end; ++current) {
+            if (!first) {
+                result << joiner;
             }
-            ++item_curr;
-            ++incl_curr;
+            result << *current;
+            first = false;
         }
-        result.emplace_back(std::move(p1), std::move(p2));
-
-        auto carry = false;
-        for (auto &&item: included) {
-            if (item == false) {
-                item = true;
-                carry = false;
-                break;
-            } else {
-                item = false;
-                carry = true;
-            }
-        }
-        if (carry) {
-            break;
-        }
+        return result.str();
     }
 
-    return result;
+    template<typename T>
+    class postorder {
+    public:
+        postorder(
+                T root,
+                const function<vector<T>(T elem)> &find_adjacent
+        ) : root_(std::move(root)), find_adjacent_(find_adjacent) {
+
+        }
+
+        struct postorder_stack_elem {
+            vector<T> elems;
+            size_t current_index{0};
+
+            [[nodiscard]] bool at_end() const { return current_index == elems.size(); }
+
+            T &current_elem() {
+                return elems.at(current_index);
+            }
+
+            bool operator==(const postorder_stack_elem &b) {
+                return elems == b.elems && current_index == b.current_index;
+            }
+        };
+
+        struct postorder_iter {
+        private:
+            stack<postorder_stack_elem> stack_{};
+            function<vector<T>(const T &elem)> find_adjacent_;
+
+            void fill_stack() {
+                if (!stack_.empty()) {
+                    while (true) {
+                        auto adj = find_adjacent_(stack_.top().current_elem());
+                        if (adj.empty()) {
+                            break;
+                        }
+                        stack_.push({adj, 0});
+                    }
+                }
+            }
+
+        public:
+            postorder_iter(stack<postorder_stack_elem> stack,
+                           function<vector<T>(const T &elem)> find_adjacent) : stack_(stack), find_adjacent_(
+                    std::move(find_adjacent)) {
+                fill_stack();
+            }
+
+            bool operator==(const postorder_iter &rhs) const {
+                return stack_ == rhs.stack_;
+            }
+
+            bool operator!=(const postorder_iter &rhs) const {
+                return !(*this == rhs);
+            }
+
+            T *operator->() {
+                if (stack_.empty()) {
+                    throw logic_error("invalid dereference");
+                }
+                postorder_stack_elem &st = stack_.top();
+                return &(st.elems.at(st.current_index));
+            };
+
+            T &operator*() {
+                if (stack_.empty()) {
+                    throw logic_error("invalid dereference");
+                }
+                return stack_.top().elems.at(stack_.top().current_index);
+            }
+
+            postorder_iter &operator++() {
+                if (!stack_.empty()) {
+                    stack_.top().current_index++;
+                    if (stack_.top().at_end()) {
+                        stack_.pop();
+                    } else {
+                        fill_stack();
+                    };
+                }
+                return *this;
+            }
+        };
+
+        [[nodiscard]] postorder_iter begin() const {
+            stack<postorder_stack_elem> s;
+            s.push({{root_}, 0});
+            return postorder_iter{std::move(s), find_adjacent_};
+        }
+
+        [[nodiscard]] postorder_iter end() const {
+            return postorder_iter{stack<postorder_stack_elem>(), find_adjacent_};
+        }
+
+    private:
+        T root_;
+        function<vector<T>(T elem)> find_adjacent_;
+    };
+
+    template<typename Iter>
+    vector<pair<set<typename iterator_traits<Iter>::value_type>,set<typename iterator_traits<Iter>::value_type>>>
+    find_partitions(const Iter &begin, const Iter &end) {
+        using namespace std;
+
+        typedef typename iterator_traits<Iter>::value_type value_type;
+
+        vector<bool> included(distance(begin, end));
+
+        vector<pair<set<value_type>,set<value_type>>> result;
+
+        while (true) {
+            set<value_type> p1;
+            set<value_type> p2;
+            Iter item_curr = begin;
+            auto incl_curr = included.begin();
+            while (item_curr != end) {
+                if (*incl_curr) {
+                    p1.insert(*item_curr);
+                } else {
+                    p2.insert(*item_curr);
+                }
+                ++item_curr;
+                ++incl_curr;
+            }
+            result.emplace_back(std::move(p1), std::move(p2));
+
+            auto carry = false;
+            for (auto &&item: included) {
+                if (item == false) {
+                    item = true;
+                    carry = false;
+                    break;
+                } else {
+                    item = false;
+                    carry = true;
+                }
+            }
+            if (carry) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    template<typename S>
+    pair<vector<S>, vector<S>> iterate_until_cycle(
+            S initial_state,
+            const function<S(const S &)> &next_state,
+            const function<bool(const S &, const S &)> &states_less) {
+        S current = std::move(initial_state);
+        vector<S> in_order;
+        map<S, size_t, function<bool(const S &, const S &)>> seen(states_less);
+
+        for (size_t idx = 0;; ++idx) {
+            auto has_been_seen = seen.find(current);
+            if (has_been_seen == seen.end()) {
+                in_order.push_back(current);
+                seen[current] = idx;
+                current = next_state(current);
+            } else {
+                auto foo = has_been_seen->second;
+                auto vec_iter = in_order.begin();
+                advance(vec_iter, foo);
+                vector<S> prefix{in_order.begin(), vec_iter};
+                vector<S> cycle{vec_iter, in_order.end()};
+                return {std::move(prefix), std::move(cycle)};
+            }
+        }
+    }
 }
